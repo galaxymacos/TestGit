f2d48be57d98dcbcffe3e52e93dac4568525edb8
galaxy <galaxymacos@gmail.com>
2018-06-04 11:45:25 -0400

    + Hello world message

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace TestGit
+﻿using System;
+
+namespace TestGit
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("hello world");
         }
     }
 }
